4d2f2ccf04973685260e55e1613a45f78cb77da9
ahmkha <ahmed.khan@stonybrook.edu>
2019-07-20 00:48:52 -0400

    Front End Skeleton

--- App.js
+++ App.js
@@ -1,19 +1,88 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import HomeTextFields from './components/HomeTextFields';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Yeet</Text>
-    </View>
-  );
+class LoginScreen extends React.Component {
+  render() {
+
+    return(
+      <View style={styles.container}>
+        <Image 
+          source = {require('./assets/ssbu_logo.png')}
+          style = {styles.logo_pic}
+        />
+        <Text> Username: </Text>
+        <HomeTextFields />
+        <Text> Password: </Text>
+        <HomeTextFields />
+        <Button 
+          title = "Login" 
+        />
+        <Button 
+          title = "Register" 
+          onPress = {() => this.props.navigation.navigate('SignUp')} 
+        />
+      </View>
+    );
+  }
+}
+
+class SignUpScreen extends React.Component {
+  render() {
+      return (
+        <View>
+          <Text> Username: </Text>
+          <HomeTextFields />
+          <Text> Password: </Text>
+          <HomeTextFields />
+          <Text> E-mail Address: </Text>
+          <HomeTextFields />
+          <Text> Address: </Text>
+          <HomeTextFields />
+          <Button 
+            title = "Register" 
+          />
+        </View>
+      );
+  }
+}
+
+const MainNavigator = createStackNavigator({
+  Login : {
+    screen: LoginScreen,
+    navigationOptions: {
+      header: null,
+    }
+  },
+
+  SignUp : {
+    screen: SignUpScreen,
+    navigationOptions: {
+      header: null,
+    }
+  },
+});
+
+const AppContainer = createAppContainer(MainNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f6f6f6',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo_pic: {
+    flex: 1,
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 });
